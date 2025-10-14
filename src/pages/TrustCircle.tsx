fa09e6058ecad297124ee20be77693c6f2273a15
chippagiri-sritha <sritha.chippagiri@gmail.com>
2025-10-15 01:27:09 +0530

    Create TrustCircle.tsx

---
+++ src/pages/TrustCircle.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import MobileLayout from '../components/layout/MobileLayout';
+import TrustCircleComponent from '../components/trust-circle/TrustCircle';
+
+const TrustCircle: React.FC = () => {
+  return (
+    <MobileLayout>
+      <TrustCircleComponent />
+    </MobileLayout>
+  );
+};
+
+export default TrustCircle;
