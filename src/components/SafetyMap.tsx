9222736544cb1c828c280d1da2352974777be44b
chippagiri-sritha <sritha.chippagiri@gmail.com>
2025-10-15 03:57:13 +0530

    Create SafetyMap.tsx

---
+++ src/components/SafetyMap.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import SafetyMapComponent from './map/SafetyMapComponent'; // Correct import path
+ 
+const SafetyMap: React.FC = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-center mt-4 text-3xl font-semibold text-gray-800">Naari Guard Map</h1>
+      <SafetyMapComponent />
+    </div>
+  );
+};
+ 
+export default SafetyMap;
