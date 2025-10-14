c19ddfa3008847eb11b00b747cb6b038abdf1ef2
chippagiri-sritha <sritha.chippagiri@gmail.com>
2025-10-15 01:26:39 +0530

    Create Settings.tsx

---
+++ src/pages/Settings.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import MobileLayout from '../components/layout/MobileLayout';
+import SettingsComponent from '../components/settings/Settings';
+
+const Settings: React.FC = () => {
+  return (
+    <MobileLayout>
+      <SettingsComponent />
+    </MobileLayout>
+  );
+};
+
+export default Settings;
